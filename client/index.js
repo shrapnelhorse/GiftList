ad483f51b648c8c55c755fe7f83fcb992ffe8bdb
adamkinney <100167362+shrapnelhorse@users.noreply.github.com>
2023-11-07 15:19:23 -0800

    added prompt, sends name and proof to server, which now verifies

--- client/index.js
+++ client/index.js
@@ -5,10 +5,22 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  const name = await new Promise(resolve => {
+    readline.question("What is your name? ", resolve)
+  })
+
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof
   });
 
   console.log({ gift });
